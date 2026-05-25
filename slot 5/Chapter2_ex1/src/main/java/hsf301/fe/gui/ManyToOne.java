42d7ea89ca7bff9bc525b541077e1699f04ef940
Binh Tran <tvtbqueson1112005@gmail.com>
2026-05-25 15:42:05 +0700

    Update exercises

---
+++ slot 5/Chapter2_ex1/src/main/java/hsf301/fe/gui/ManyToOne.java
@@ -0,0 +1,17 @@
+package hsf301.fe.gui;
+
+import hsf301.fe.configs.AppConfig;
+import hsf301.fe.pojos.Student;
+import hsf301.fe.services.StudentService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class ManyToOne {
+    public static void main(String[] args) {
+        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+        StudentService myService = context.getBean(StudentService.class);
+        Student st = new Student("Lam", "Nguyen", 8);
+
+        myService.save(st);
+    }
+}
